68d4fcd28c6c55d507b46421a5ad8a456fc9048e
danieldelacruzredondo <47848246+danieldelacruzredondo@users.noreply.github.com>
2023-11-22 12:44:09 +0100

    Update SYNCHRNZR.vhd

--- SYNCHRNZR.vhd
+++ SYNCHRNZR.vhd
@@ -3,38 +3,26 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity SYNCHRNZR is
  port (
  CLK : in std_logic;
- ASYNC1_IN : in std_logic;
- ASYNC2_IN: in std_logic;
- ASYNC3_IN : in std_logic;
- ASYNC4_IN : in std_logic;
- SYNC_OUT1 : out std_logic;
- SYNC_OUT2 : out std_logic;
- SYNC_OUT3 : out std_logic;
- SYNC_OUT4 : out std_logic
+ ASYNC_IN : in std_logic;
+ RESET: IN std_logic;
+ SYNC_OUT : out std_logic
+ 
+
  );
 end SYNCHRNZR;
 architecture BEHAVIORAL of SYNCHRNZR is
- signal sreg1 : std_logic_vector(1 downto 0);
- signal sreg2 : std_logic_vector(1 downto 0);
- signal sreg3 : std_logic_vector(1 downto 0);
- signal sreg4 : std_logic_vector(1 downto 0);
+ signal sreg : std_logic_vector(1 downto 0);
  
 begin
- process (CLK)
+ process (CLK, reset)
  begin
- if rising_edge(CLK) then
- sync_out1 <= sreg1(1);
- sreg1 <= sreg1(0) & async1_in;
- 
- sync_out2 <= sreg2(1);
- sreg2 <= sreg2(0) & async2_in;
- 
- sync_out3 <= sreg3(1);
- sreg3 <= sreg3(0) & async3_in;
- 
- sync_out4 <= sreg4(1);
- sreg4 <= sreg4(0) & async4_in;
- 
+ if(reset='0') then
+    sync_out<= '0';
+    sreg <="00";
+ elsif rising_edge(CLK) then
+ sync_out <= sreg(1);
+ sreg <= sreg(0) & async_in;
+  
  end if;
  end process;
 end BEHAVIORAL;
