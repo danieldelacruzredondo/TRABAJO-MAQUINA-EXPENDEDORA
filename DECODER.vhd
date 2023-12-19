3d0c9c6b9cdf0501da0ca2c2559a0e95edb30979
danieldelacruzredondo <47848246+danieldelacruzredondo@users.noreply.github.com>
2023-12-19 19:58:42 +0100

    Update DECODER.vhd

--- DECODER.vhd
+++ DECODER.vhd
@@ -2,57 +2,71 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 USE ieee.std_logic_arith.ALL;
 USE ieee.std_logic_unsigned.ALL;
+
 ENTITY decoder IS
 PORT (
 
-code : IN std_logic_vector(3 DOWNTO 0);
-segment1 : OUT std_logic_vector(6 DOWNTO 0);  --display mas a la derecha
-segment2 : OUT STD_LOGIC_VECTOR (6 downto 0); --segundo display
-segment3: OUT STD_LOGIC_VECTOR (6 DOWNTO 0)   --tercer display
+CLK: IN STD_LOGIC;
+RESET: IN STD_LOGIC;
+CODE : IN std_logic_vector(3 DOWNTO 0);
+anodo : OUT std_logic_vector(2 DOWNTO 0);
+segment : OUT STD_LOGIC_VECTOR (6 downto 0);
+dt : OUT std_logic
 );
 END ENTITY decoder;
+
+
 ARCHITECTURE dataflow OF decoder IS
-
-signal signal1: std_logic_vector (6 downto 0);
-signal signal2: std_logic_vector (6 downto 0);
-signal signal3: std_logic_vector (6 downto 0);
-
+signal display_activation : std_logic_vector(1 DOWNTO 0); --para activar el display
+signal refresh_count : std_logic_vector(19 DOWNTO 0);
+signal LED_BCD : std_logic_vector(3 DOWNTO 0);
+signal displayed_numbers : std_logic_vector(11 DOWNTO 0); --para meter los 4 bits a cada display
 BEGIN
 
-WITH code SELECT
-signal1<=  "0000001" WHEN "0000",
-        "0000001" WHEN "0001",
-        "0000001" WHEN "0010",
-        "0000001" WHEN "0011",
-        "0000001" WHEN "0100",
-        "0000001" WHEN "0101",
-        "0000001" WHEN "0110",
-        "0000001" WHEN "0111",
-        "0000001" WHEN "1000",
-        "0000001" WHEN "1001",
-        "0000001" WHEN "1010",
-        "1111110" WHEN others;
-        
-WITH code select
-signal2 <= "0000001" WHEN "0000",
-        "1001111" WHEN "0001",
-        "0010010" WHEN "0010",
-        "0000110" WHEN "0011",
-        "1001100" WHEN "0100",
-        "0100100" WHEN "0101",
-        "0100000" WHEN "0110",
-        "0001111" WHEN "0111",
-        "0000000" WHEN "1000",
-        "0000100" WHEN "1001",
-        "0000001" WHEN "1010",
-        "1111110" WHEN others;
-        
-WITH code select
-signal3 <= "1001111" WHEN "1010",
-        "1111110" WHEN others;
+process(LED_BCD)
+BEGIN
+    case LED_BCD is -- señal de decenas de centimo: de 0 a 9 hasta los 90 cents y de 0 a 4 hasta el 1.4$  
+    WHEN "0000" => segment <= "0000001";
+    WHEN "0001" => segment <= "1001111";
+    WHEN "0010" => segment <= "0010010";
+    WHEN "0011" => segment <= "0000110";
+    WHEN "0100" => segment <= "1001100";
+    WHEN "0101" => segment <= "0100100";
+    WHEN "0110" => segment <= "0100000";
+    WHEN "0111" => segment <= "0001111";
+    WHEN "1000" => segment <= "0000000";
+    WHEN "1001" => segment <= "0000100";
+    WHEN others => segment <= "1111110";
+    end case;
+end process;
 
-segment1<=signal1;
-segment2<=signal2;
-segment3<=signal3;
-          
+process(clk, reset)
+BEGIN
+    if (reset='1') then
+       refresh_count <= (others => '0');
+    else if (rising_edge(clk)) then
+       refresh_count <= refresh_count + 1;
+    end if;
+    end if;
+end process;
+display_activation <= refresh_count (19 DOWNTO 18);
+LED_BCD <= code;
+
+process(display_activation)
+BEGIN
+   case display_activation is
+   when "00" =>
+      anodo <= "011";
+      dt<='1';
+      LED_BCD <= displayed_numbers(11 DOWNTO 8);
+   when "01" =>
+      anodo <= "101";
+      LED_BCD <= displayed_numbers(7 DOWNTO 4);
+   when "10" =>
+      anodo <= "110";
+      LED_BCD <= displayed_numbers(3 DOWNTO 0);
+   when "11" =>
+      anodo<="111";
+   end case;
+end process;
 END ARCHITECTURE dataflow;
