5cb358c87f5baa50561ff6735f11854504b7a546
danieldelacruzredondo <47848246+danieldelacruzredondo@users.noreply.github.com>
2023-12-21 20:43:07 +0100

    Update TOP.vhd

--- TOP.vhd
+++ TOP.vhd
@@ -10,12 +10,17 @@ entity TOP is
     CLK: IN std_logic;
     RESET: IN STD_LOGIC;
     PRODUCTO: IN STD_LOGIC_VECTOR (3 downto 0);
-    DIGSEL: out STD_LOGIC_VECTOR (2 downto 0);  
+    REC_PROD: IN STD_LOGIC;
+    DIGSEL: out STD_LOGIC_VECTOR (7 downto 0);  
     ERROR: OUT STD_LOGIC;
+    ESTADO0: OUT STD_LOGIC;
+    ESTADO1: OUT STD_LOGIC;
+    ESTADO2: OUT STD_LOGIC;
     VENTA: OUT STD_LOGIC;
     LED_PROD: OUT STD_LOGIC_VECTOR (15 downto 12);
     SEGMENT: OUT std_logic_vector (6 downto 0);
     DP: OUT STD_LOGIC
+    
         
     );
 end TOP;
@@ -24,7 +29,7 @@ architecture structural of TOP is
 
 signal moneda: std_logic_vector (3 downto 0);
 
-signal edge_sal : std_logic_vector (3 downto 0);
+signal edge_salida : std_logic_vector (3 downto 0);
 signal code: std_logic_vector (3 downto 0);
 signal sigerr: std_logic;
 signal sigven: std_logic;
@@ -45,7 +50,7 @@ END COMPONENT;
 component COUNTER  PORT(
     CLK: IN STD_LOGIC;
     RESET: IN STD_LOGIC;    
-    BOTON: IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+    BOTON: IN STD_LOGIC_VECTOR (3 DOWNTO 0);   
     DINERO_TOT: OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
     IMP_EXACTO: OUT STD_LOGIC
 );
@@ -56,10 +61,14 @@ component maquina_expendedora  PORT(
     RESET: IN STD_LOGIC;
     BOTON: IN STD_LOGIC_VECTOR (3 downto 0); --Dinero que vamos introduciendo.
     CUENTA: IN STD_LOGIC; -- La pondremos a '1' cuando se retorne 1,40$ desde el counter.
-    SELECC_PROD: IN STD_LOGIC_VECTOR (3 downto 0);                         
+    SELECC_PROD: IN STD_LOGIC_VECTOR (3 downto 0);  
+    REC_PROD: IN STD_LOGIC;                       
     LED_ERROR: OUT STD_LOGIC;
     LED_VENTA: OUT STD_LOGIC;
-    LED_PROD: OUT STD_LOGIC_VECTOR (3 DOWNTO 0)
+    LED_PROD: OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
+    ESTADO0: OUT STD_LOGIC;
+    ESTADO1: OUT STD_LOGIC;
+    ESTADO2: OUT STD_LOGIC
     
 );
 end component;
@@ -67,10 +76,11 @@ end component;
 component decoder PORT(
 CLK: IN STD_LOGIC;
 RESET: IN STD_LOGIC;
-CODE : IN std_logic_vector(3 DOWNTO 0);
-anodo : OUT std_logic_vector(2 DOWNTO 0);
+code : IN std_logic_vector(3 DOWNTO 0);
+anodo : OUT std_logic_vector(7 DOWNTO 0);
 segment : OUT STD_LOGIC_VECTOR (6 downto 0);
-dt : OUT std_logic
+DP: OUT STD_LOGIC
+
 
 );
 end component;
@@ -84,14 +94,20 @@ Inst_GEST_INT: GEST_INT PORT MAP(
     BTND=>BTND,   
     CLK=>clk,
     RESET=>reset,
-    EDGE_SAL=>edge_sal
+    EDGE_SAL(0)=>edge_salida(0),
+    EDGE_SAL(1)=>edge_salida(1),
+    EDGE_SAL(2)=>edge_salida(2),
+    EDGE_SAL(3)=>edge_salida(3)
 );
-
 
 Inst_counter: counter PORT MAP(
     CLK=>CLK,
-    RESET=>RESET,   
-    BOTON=>edge_sal,
+    RESET=>RESET,  
+     
+    BOTON(0)=>edge_salida(0),
+    BOTON(1)=>edge_salida(1),
+    BOTON(2)=>edge_salida(2),
+    BOTON(3)=>edge_salida(3),
     DINERO_TOT=>code, -- Señal que sale del counter para relacionarlo con el decoder
     IMP_EXACTO=>importe_exacto   
 );
@@ -99,12 +115,19 @@ Inst_counter: counter PORT MAP(
 Inst_maquina_expendedora: MAQUINA_EXPENDEDORA PORT MAP (
     CLK=>CLK,
     RESET=>RESET,
-    BOTON=>edge_sal,
+    BOTON(0)=>edge_salida(0),
+    BOTON(1)=>edge_salida(1),
+    BOTON(2)=>edge_salida(2),
+    BOTON(3)=>edge_salida(3),
     CUENTA=>importe_exacto,
-    SELECC_PROD=>PRODUCTO,                        
+    SELECC_PROD=>PRODUCTO,  
+    REC_PROD=>REC_PROD,                      
     LED_ERROR => ERROR,
     LED_VENTA => VENTA,
-    LED_PROD => LED_PROD
+    LED_PROD => LED_PROD,
+    ESTADO0=> ESTADO0,
+    ESTADO1=>ESTADO1,
+    ESTADO2=>ESTADO2
        
 );
 
@@ -114,9 +137,9 @@ Inst_decoder: decoder PORT MAP (
     CODE => code,
     anodo =>DIGSEL,
     segment =>SEGMENT,
-    dt =>DP
+    DP=>DP
+   
 );
     
 
 end structural;
-
