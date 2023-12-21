a78537ddefcfba3c30c47846f26e8153d0d05eb9
danieldelacruzredondo <47848246+danieldelacruzredondo@users.noreply.github.com>
2023-12-21 20:48:03 +0100

    Update MAQUINA_EXPENDEDORA.vhd

--- MAQUINA_EXPENDEDORA.vhd
+++ MAQUINA_EXPENDEDORA.vhd
@@ -6,21 +6,25 @@ entity MAQUINA_EXPENDEDORA is
     CLK: IN STD_LOGIC;
     RESET: IN STD_LOGIC;
     BOTON: IN STD_LOGIC_VECTOR (3 downto 0); --Dinero que vamos introduciendo.
-    CUENTA: IN STD_LOGIC; -- La pondremos a '1' cuando se retorne 1,40$ desde el counter.
-    SELECC_PROD: IN STD_LOGIC_VECTOR (3 downto 0); 
-    --VENDER: IN STD_LOGIC;                      
+    CUENTA: IN STD_LOGIC:='0'; -- La pondremos a '1' cuando se retorne 1,40$ desde el counter.
+    SELECC_PROD: IN STD_LOGIC_VECTOR (3 downto 0); -- Seleccionar un producto
+    REC_PROD : IN STD_LOGIC;                     
     LED_ERROR: OUT STD_LOGIC;
     LED_VENTA: OUT STD_LOGIC;
-    LED_PROD: OUT STD_LOGIC_VECTOR (3 DOWNTO 0)
+    LED_PROD: OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
+    ESTADO0: OUT STD_LOGIC;
+    ESTADO1: OUT STD_LOGIC;
+    ESTADO2: OUT STD_LOGIC
+    
                                               
      );
 end MAQUINA_EXPENDEDORA;
 
 architecture Behavioral of MAQUINA_EXPENDEDORA is
 type STATES is (S0, --Estado de reposo/error. led error encendido
-                S1, -- Metemos dinero. llega una pulsación de boton
-                S2, -- Seleccionamos un producto
-                S3  -- Seleccionado el producto e importe de 1.40$. LED VENTA.
+                S1, -- Seleccionamos un producto
+                S2, -- Metemos dinero. llega una pulsación de boton
+                S3  -- Seleccionado el producto e importe de 1.40$.SW para recoger producto. LED VENTA.
                );
  signal current_state: STATES := S0;
  signal next_state: STATES;
@@ -45,100 +49,66 @@ begin
         	when S0=>
         	--Seleccionamos uno de los 4 productos ->
             	if SELECC_PROD = "0001" then
-                	next_state<=S2; 
+                	next_state<=S1; 
                 elsif SELECC_PROD = "0010" then
-                    next_state<=S2; 
+                    next_state<=S1; 
                 elsif SELECC_PROD = "0100" then
-                    next_state<=S2; 
+                    next_state<=S1; 
                 elsif SELECC_PROD = "1000" then
-                    next_state<=S2;
-            --Pulsamos uno de los 4 botones para meter dinero ->
-                elsif BOTON = "0001" then   
                     next_state<=S1;
-                elsif BOTON = "0010" then   
-                    next_state<=S1;  
-                elsif BOTON = "0100" then   
-                    next_state<=S1;  
-                elsif BOTON = "1000" then   
-                    next_state<=S1;
-                end if;
+                    end if;                            
             
-            when S1=> 
-             -- Si el importe esta bien, pasamos al estado de venta
-                if CUENTA = '1' then
-                
-                    next_state<=S3;
-                -- Seguimos metiendo dinero, seguimos en el mismo estado
-                elsif BOTON = "0001" then   
-                    next_state<=S1;
-                elsif BOTON = "0010" then   
-                    next_state<=S1;  
-                elsif BOTON = "0100" then   
-                    next_state<=S1;  
-                elsif BOTON = "1000" then   
-                    next_state<=S1;
-                -- Habiendo metido dinero, seleccionamos producto
-                elsif SELECC_PROD = "0001" then
-                	next_state<=S2; 
-                elsif SELECC_PROD = "0010" then
-                    next_state<=S2; 
-                elsif SELECC_PROD = "0100" then
-                    next_state<=S2; 
-                elsif SELECC_PROD = "1000" then
-                    next_state<=S2;
-                end if;
-                
-                  
-             when S2 => 
-             -- Si hay un producto selleccionado, y el importe esta bien, hacemos la venta
-                if CUENTA = '1' then
-                    if SELECC_PROD = "0001" then
-                        next_state<=S3;
-                    elsif SELECC_PROD = "0010" then
-                       next_state<=S3;
-                    elsif SELECC_PROD = "0100" then
-                        next_state<=S3;
-                    elsif SELECC_PROD = "1000" then                        
-                    next_state<=S3;
-                    end if;
-                end if;  
-             -- Metemos dinero habiendo seleccionado un producto previamente
+            when S1=> -- Estando con un producto seleccionado, introducimos dinero
+                if SELECC_PROD = "0000" then
+                    next_state<=S0;   
+                end if;       
                 if BOTON = "0001" then   
-                    next_state<=S1;
-                elsif BOTON = "0010" then   
-                    next_state<=S1;  
-                elsif BOTON = "0100" then   
-                    next_state<=S1;  
-                elsif BOTON = "1000" then   
-                    next_state<=S1;
-                
-               -- Cambiamos de producto
-                elsif SELECC_PROD = "0001" then
-                	next_state<=S2; 
-                elsif SELECC_PROD = "0010" then
-                    next_state<=S2; 
-                elsif SELECC_PROD = "0100" then
-                    next_state<=S2; 
-                elsif SELECC_PROD = "1000" then
                     next_state<=S2;
+                elsif BOTON = "0010" then   
+                    next_state<=S2;  
+                elsif BOTON = "0100" then   
+                    next_state<=S2;  
+                elsif BOTON = "1000" then   
+                    next_state<=S2;
+                end if;                
+                                                 
+             when S2 => 
+             
+                if CUENTA = '1' then
+                -- Si  seguimos metiendo dinero estando todo bien, volvemos a reposo
+                    if BOTON = "0001" then   
+                        next_state<=S0;
+                    elsif BOTON = "0010" then   
+                        next_state<=S0;  
+                    elsif BOTON = "0100" then   
+                        next_state<=S0;  
+                    elsif BOTON = "1000" then   
+                        next_state<=S0;
+                end if;                      
+                end if; 
+                
+             if CUENTA='1' then            
+                next_state<=S3;
+             end if;
+                               
+                    
+             -- No hemos llegado al 1,40
+             if CUENTA='0' then
+                if BOTON = "0001" then   
+                    next_state<=S2;
+                elsif BOTON = "0010" then   
+                    next_state<=S2;  
+                elsif BOTON = "0100" then   
+                    next_state<=S2;  
+                elsif BOTON = "1000" then   
+                    next_state<=S2;                               
+                end if;
                 end if;
                                                   
               when S3=> 
-              -- Se realiza la venta y pasamos al estado inicial
-                --if VENDER = '1' then
-                    --next_state<=S0;
-                if SELECC_PROD = "0000" then -- Si no hay producto selecc -> volvemos a reposo
-                	next_state<=S0;
-               --Si estamos listos para venta y se mete mas dinero volvemos al estado inicial
-                elsif BOTON = "0001" then   
-                    next_state<=S0;
-                elsif BOTON = "0010" then   
-                    next_state<=S0;  
-                elsif BOTON = "0100" then   
-                    next_state<=S0;  
-                elsif BOTON = "1000" then   
-                    next_state<=S0;                	                        
-                end if;                                                                                                         
+                   if REC_PROD = '1' THEN 
+                        next_state<=S0;  
+                   end if;                                                                                                    
             
             when others=>
             	next_state<=S0;
@@ -155,6 +125,9 @@ begin
             	LED_PROD <= "0000";
             	LED_ERROR <= '1';
             	LED_VENTA <='0';
+            	ESTADO0<='1'; -- Estado reposo
+            	ESTADO1<='0'; -- Estado producto seleccionado
+            	ESTADO2<='0'; -- Estado meter dinero
                                
             when S1=>  
               
@@ -170,6 +143,9 @@ begin
           		         		
             	LED_VENTA <='0';
             	LED_ERROR <='1';
+            	ESTADO0<='0';
+            	ESTADO1<='1';
+            	ESTADO2<='0';
                 
             when S2=>
             	if SELECC_PROD = "0001" then
@@ -183,6 +159,9 @@ begin
                     end if;
             	LED_ERROR <='1';
             	LED_VENTA <='0';
+            	ESTADO0<='0';
+            	ESTADO1<='0';
+            	ESTADO2<='1';
             
             when S3=>
                     if SELECC_PROD = "0001" then
@@ -197,13 +176,19 @@ begin
             	
             	LED_VENTA<='1';
             	LED_ERROR<='0';
+            	ESTADO0<='0';
+            	ESTADO1<='0';
+            	ESTADO2<='0';
             	                                 	
             when others=>
             	LED_PROD <= "0000";
             	LED_ERROR <='1';
             	LED_VENTA <='0';
+            	ESTADO0<='0';
+            	ESTADO1<='0';
+            	ESTADO2<='0';
+            	
             	           	           
         end case;
-    end process;
-    
+    end process;   
 end Behavioral;
